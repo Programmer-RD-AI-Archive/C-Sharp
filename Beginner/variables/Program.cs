931aaf62fa2ac041c9c5fa1d9f67c269f20c3ddb
Programmer-RD-AI <go2ranuga@gmail.com>
2022-07-28 21:22:44 +0530

    Update Program.cs
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

--- Beginner/variables/Program.cs
+++ Beginner/variables/Program.cs
@@ -12,18 +12,8 @@ namespace important
     {
         static void Main(string[] args)
         {
-            string characterName = "Nani";
-            Console.WriteLine($"There once was a man named {characterName}");
-            int random_string =  Convert.ToInt32(Console.ReadLine());
-            int[] array = {0, 1, 2};
-            string[] friends = {"test","grgr","grgr"};
-            friends[0] = "test";
-            Console.WriteLine($"{friends[1]}");
-            Console.WriteLine($"{input()}");
+            
         }
-        public static string input(){
-            string whatever = Console.ReadLine();
-            return whatever;
-        }
+        
     }
 }
